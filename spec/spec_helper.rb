ed111740b9fc85711814280a3e54b120e0bfd67f
Chae <chaemobile@gmail.com>
2014-10-12 10:26:38 -0400

    Extracts rspec gem configs into support folder

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,11 +6,9 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require "rspec/rails"
 
-Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |file| require file }
 
 RSpec.configure do |config|
-
-  config.include FactoryGirl::Syntax::Methods
 
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
