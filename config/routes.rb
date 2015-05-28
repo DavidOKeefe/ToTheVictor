8c6edb506fe5f94493dd790478c516a3e4e36db6
Chae <chaemobile@gmail.com>
2015-05-27 20:21:06 -0400

    Add dashboard index
    * When a user successfully signs in, they are
      redirected to the dashboard index

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
 
   devise_scope :user do
-    root to: 'devise/sessions#new'
+    unauthenticated :user do
+      root to: 'devise/sessions#new', as: :unauthenticated_root
+    end
+
+    authenticated :user do
+      root to: 'dashboard#index', as: :authenticated_root
+    end
   end
 end
