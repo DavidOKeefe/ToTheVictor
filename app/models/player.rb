78acf06b819bfef19a0502c57a6c95f86f942458
David OKeefe <davidpokeefe3@gmail.com>
2015-06-13 18:26:29 -0400

    Created join between player and scorecard
    * Added scorecard_id to players table
    * Added belongs_to to players model
    * Added has_many to scorecard model
    * Created factorygirl spec for player

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,3 +1,4 @@
 class Player < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
+  belongs_to :scorecard
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150613185115) do
+ActiveRecord::Schema.define(version: 20150613215456) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "players", force: true do |t|
-    t.string   "name",       null: false
+    t.string   "name",         null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "scorecard_id"
   end
 
   create_table "scorecards", force: true do |t|
