a9e46cfe4446fa0659d4e7da973c66e6063dcd5c
Chae <chaemobile@gmail.com>
2015-06-13 16:21:17 -0400

    Change name of make to built?

--- app/models/scorecard_player_builder.rb
+++ app/models/scorecard_player_builder.rb
@@ -2,12 +2,19 @@ class ScorecardPlayerBuilder
   include ActiveModel::Model
 
   attr_accessor :name
+  attr_reader :player
 
   def initialize(params)
     @params = params
   end
 
-  def make
-    Player.create(name: 'sally')
+  def built?
+    player.persisted?
+  end
+
+  private
+
+  def player
+    @player ||= Player.create(name: 'sally')
   end
 end
--- spec/models/scorecard_player_builder_spec.rb
+++ spec/models/scorecard_player_builder_spec.rb
@@ -3,11 +3,9 @@ require 'spec_helper'
 describe ScorecardPlayerBuilder do
   let(:scorecard_player_builder) { described_class.new([]) }
 
-  describe '#make' do
-    it 'persists a player' do
-      expect{
-        scorecard_player_builder.make
-      }.to change{Player.all.size}.by 1
+  describe '#built?' do
+    it 'returns true if player is persisted' do
+      expect(scorecard_player_builder.built?).to be_truthy
     end
   end
 end
