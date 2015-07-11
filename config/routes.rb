d04942ae7d6559c1cbfaec17f8aa872e9ce4e768
David OKeefe <davidpokeefe3@gmail.com>
2015-07-11 09:21:14 -0400

    Updated routes with scorecards resources

--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,5 @@ Rails.application.routes.draw do
 
   resources :players, only: [:index, :edit, :update, :destroy]
   resource :scorecard_player_builders, only: [:new, :create]
+  resources :scorecards, only: [:index, :new, :create]
 end
