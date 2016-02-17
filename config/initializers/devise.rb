db32c19c67741b34693ba2de1c809114eda55eba
David OKeefe <davidpokeefe3@gmail.com>
2016-02-17 11:32:13 -0500

    Adds signout
    * Configured devise for less code in signout link in partial
    * Extracted partial for signout content (what we consider header information)
    * Adds feature spec for signing out

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -3,6 +3,9 @@ Devise.setup do |config|
   # ORM configuration
   require 'devise/orm/active_record'
 
+  # https://github.com/plataformatec/devise/wiki/How-To:-Add-sign_in,-sign_out,-and-sign_up-links-to-your-layout-template
+  config.sign_out_via = :get
+
   # Authentication
   config.case_insensitive_keys = [ :email ]
   config.strip_whitespace_keys = [ :email ]
